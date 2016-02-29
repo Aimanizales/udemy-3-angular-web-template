2f37012c87f8bbb241b4e52082f1977701bcfdb8
Aiman Abdel-Samad <aiman.samad@zemoga.com>
2016-02-29 15:12:14 -0500

    Finished Section 3 tutorial: JSON data http

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -4,10 +4,38 @@ angular.module('templateStore.templates', ['ngRoute'])
 	$routeProvider
 		.when('/templates', {
 			templateUrl: 'templates/templates.html',
-			controller: 'TemplateCtrl'
+			controller: 'TemplatesCtrl'
+		})
+		.when('/templates/:templateId', {
+			templateUrl: 'templates/template-details.html',
+			controller: 'TemplateDetailsCtrl'
 		});
 }])
 
-.controller('TemplatesCtrl', ['$scope', function($scope){
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
 	console.log('TemplatesCtrl Initialized');
+	$http.get('json/templates.json').success(function (data) {
+		$scope.templates = data;
+		console.log('TemplatesCtrl data', $scope.templates);
+	});
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+	console.log('TemplateDetailsCtrl Initialized');
+	var templateId = $routeParams.templateId;
+	$http.get('json/templates.json').success(function (response) {
+		$scope.template = $filter('filter')(response, function(d){
+			return d.id == templateId;
+		})[0];
+		$scope.name = $scope.template.name;
+		$scope.mainImage = $scope.template.images[0].name;
+		$scope.price = $scope.template.price;
+		$scope.description = $scope.template.description;
+		console.log('TemplateDetailsCtrl mainImage', $scope);
+
+	});
+
+	$scope.setImage = function (image) {
+		$scope.mainImage = image.name;
+	}
 }])
